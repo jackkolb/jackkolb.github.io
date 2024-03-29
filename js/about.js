5e79e12c50c3b1b72cc846edc823129dae88a8e4
Jack Kolb <jack@jackkolb.com>
2024-03-29 02:21:26 -0400

    Added a cool link highlight

--- js/about.js
+++ js/about.js
@@ -7,6 +7,21 @@ function handleAbout() {
 	createTimeline();
 	showBar("about-button");
 	about_active = true;
+
+	// set the handler for the link hovers
+	let idx = 0
+    document.querySelectorAll('a.bio-link').forEach(function(element) {
+		var computedStyle = window.getComputedStyle(element);
+		element.id = "link" + idx
+		let style = document.createElement("style")
+		let color = computedStyle.getPropertyValue('color')
+		let backgroundColor = computedStyle.getPropertyValue('background-color')
+		styleText = document.createTextNode("#" + element.id + ":hover { background-color: " + color + "; color: white; padding: 0 .5rem 0 .5rem; };")
+        style.appendChild(styleText)
+		element.appendChild(style)
+		idx++
+    })
+
 	return;
 }
 
