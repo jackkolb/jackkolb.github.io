64288147b766b993b6f780a6c873a3631467796e
Jack Kolb <jack@jackkolb.com>
2024-07-25 21:10:16 -0400

    updated timeline and PDFs

--- js/timeline.js
+++ js/timeline.js
@@ -1,10 +1,26 @@
 function createTimeline() {
-    timeline = [
+    timeline = [        
+        {
+            type: "Conference",
+            location: "IEEE/RSJ IROS",
+            title: "Inferring Belief States in Partially-Observable Human-Robot Teams",
+            // link: "https://jackkolb.com/pdf/DASC_24_Towards_Safe_Collaboration_Between_Autonomous_Pilots_and_Human_Crews_for_Intelligence_Surveillance_and_Reconnaissance.pdf",
+            month: "Oct",
+            year: 2024,
+        },
+        {
+            type: "Symposium",
+            location: "IEEE ICRA40",
+            title: "A Framework for Inferring Belief States in Partially-Observable Human-Robot Teams",
+            // link: "https://jackkolb.com/pdf/DASC_24_Towards_Safe_Collaboration_Between_Autonomous_Pilots_and_Human_Crews_for_Intelligence_Surveillance_and_Reconnaissance.pdf",
+            month: "Sep",
+            year: 2024,
+        },
         {
             type: "Conference",
             location: "AIAA/IEEE DASC",
             title: "Towards Safe Collaboration Between Autonomous Pilots and Human Crews for Intelligence, Surveillance, and Reconnaissance",
-            // link: "https://jackkolb.com/pdf/DASC_24_Towards_Safe_Collaboration_Between_Autonomous_Pilots_and_Human_Crews_for_Intelligence_Surveillance_and_Reconnaissance.pdf",
+            link: "https://jackkolb.com/pdf/DASC_24_Towards_Safe_Collaboration_Between_Autonomous_Pilots_and_Human_Crews_for_Intelligence_Surveillance_and_Reconnaissance.pdf",
             month: "Sep",
             year: 2024,
         },
@@ -12,8 +28,15 @@ function createTimeline() {
             type: "Conference",
             location: "AIAA Aviation",
             title: "Run Time Assurance and Human AI Fluency in Manned Autonomous Intelligence Surveillance and Reconnaissance",
-            // link: "https://jackkolb.com/pdf/Aviation_24_Run_Time_Assurance_and_Human_AI_Fluency_in_Manned_Autonomous_Intelligence_Surveillance_and_Reconnaissance.pdf",
+            link: "https://jackkolb.com/pdf/Aviation_24_Run_Time_Assurance_and_Human_AI_Fluency_in_Manned_Autonomous_Intelligence_Surveillance_and_Reconnaissance.pdf",
             month: "Jul",
+            year: 2024,
+        },
+        {
+            type: "Award",
+            award: "Successfully passed my PhD Proposal!",
+            link: "https://jackkolb.com/pdf/Kolb_Proposal_Presentation.pdf",
+            month: "Feb",
             year: 2024,
         },
         {
@@ -21,13 +44,6 @@ function createTimeline() {
             location: "AIAA SciTech",
             title: "Impact of Abstraction Levels of Context Information on AI-Advised Decision Making for an Entry Descent and Landing Task",
             link: "https://jackkolb.com/pdf/SciTech_24_Impact_of_Abstraction_Levels_of_Context_Information_on_AI_Advised_Decision_Making_for_an_Entry_Descent_and_Landing_Task.pdf",
-            month: "Jan",
-            year: 2024,
-        },
-        {
-            type: "Award",
-            award: "Successfully passed my PhD Proposal!",
-            link: "https://jackkolb.com/pdf/Kolb_Proposal_Presentation.pdf",
             month: "Jan",
             year: 2024,
         },
@@ -175,7 +191,7 @@ function createTimeline() {
         }
         
         // workshop publication
-        if (timeline[i].type == "Workshop") {
+        if (timeline[i].type == "Workshop" || timeline[i].type == "Symposium") {
             if (timeline[i].link) {
                 entry.innerHTML += '<span style="float:right"><svg class="research-paper-icon" style="width:21px;height:21px" viewBox="0 0 384 512"><path d="M320 464C328.8 464 336 456.8 336 448V416H384V448C384 483.3 355.3 512 320 512H64C28.65 512 0 483.3 0 448V416H48V448C48 456.8 55.16 464 64 464H320zM256 160C238.3 160 224 145.7 224 128V48H64C55.16 48 48 55.16 48 64V192H0V64C0 28.65 28.65 0 64 0H229.5C246.5 0 262.7 6.743 274.7 18.75L365.3 109.3C377.3 121.3 384 137.5 384 154.5V192H336V160H256zM88 224C118.9 224 144 249.1 144 280C144 310.9 118.9 336 88 336H80V368C80 376.8 72.84 384 64 384C55.16 384 48 376.8 48 368V240C48 231.2 55.16 224 64 224H88zM112 280C112 266.7 101.3 256 88 256H80V304H88C101.3 304 112 293.3 112 280zM160 240C160 231.2 167.2 224 176 224H200C226.5 224 248 245.5 248 272V336C248 362.5 226.5 384 200 384H176C167.2 384 160 376.8 160 368V240zM192 352H200C208.8 352 216 344.8 216 336V272C216 263.2 208.8 256 200 256H192V352zM336 224C344.8 224 352 231.2 352 240C352 248.8 344.8 256 336 256H304V288H336C344.8 288 352 295.2 352 304C352 312.8 344.8 320 336 320H304V368C304 376.8 296.8 384 288 384C279.2 384 272 376.8 272 368V240C272 231.2 279.2 224 288 224H336z"></path></svg><\span>'
             }
