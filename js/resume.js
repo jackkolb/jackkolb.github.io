15fd3fb68a5225c78a402c6fb52b990e7950b7f2
Jack Kolb <jack@jackkolb.com>
2026-03-30 21:20:39 -0700

    Updated CV

--- js/resume.js
+++ js/resume.js
@@ -1,9 +1,7 @@
 // javascript functions for resume tab
 
-
 function handleCV() {
-	var resume_win = window.open("resume/Kolb CV 2025.pdf", "_blank");
+	var resume_win = window.open("resume/Kolb CV 2026.pdf", "_blank");
 	resume_win.focus();
 	return;
 }
-	
